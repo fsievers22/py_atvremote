1d819103ec39219acfaab01af8a81a5a4a286dcc
Fabian Sievers <fabian.sievers@outlook.com>
2022-11-19 00:47:24 +0100

    rework

--- compile_proto.sh
+++
@@ -1,5 +0,0 @@
-SRC_FILE="./atvremote/pair/proto/pairing.proto"
-DST_DIR="./"
-./protoc/bin/protoc $SRC_FILE --python_out=$DST_DIR --mypy_out=$DST_DIR
-SRC_FILE="./atvremote/remote/proto/commands.proto"
-./protoc/bin/protoc $SRC_FILE --python_out=$DST_DIR --mypy_out=$DST_DIR
